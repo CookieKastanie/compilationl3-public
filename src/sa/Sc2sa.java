90837be3d4481f2c72ffece0fc2f91609779a964
JeremyAndre <jerem0205@yahoo.fr>
2020-02-07 18:27:14 +0100

    bruh momento

--- src/sa/Sc2sa.java
+++ src/sa/Sc2sa.java
@@ -4,21 +4,33 @@ import sc.analysis.Analysis;
 import sc.analysis.DepthFirstAdapter;
 import sc.node.*;
 
-public class Sc2sa extends DepthFirstAdapter {
+import java.sql.SQLOutput;
+
+public class Sc2sa extends DepthFirstAdapter/* implements Analysis*/{
     private SaNode returnValue;
 
+    public SaNode getRoot() {
+        return returnValue;
+    }
+
+
+    // A
     @Override
-    public void caseAProgrammeProgramme(AProgrammeProgramme node) {
+    public void caseAWithVarProgram(AWithVarProgram node) {
         SaLDec variables = null;
         SaLDec fonctions = null;
 
-        if(node.getDecVarGlob() != null) {
-            node.getDecVarGlob().apply(this);
+        if(node.getDecLVar() != null) {
+            node.getDecLVar().apply(this);
             variables = (SaLDec) returnValue;
         }
 
-        if(node.getDecListFonc() != null) {
-            node.getDecListFonc().apply(this);
+        if(node.getPv() != null) {
+            node.getPv().apply(this);
+        }
+
+        if(node.getDecLFunc() != null) {
+            node.getDecLFunc().apply(this);
             fonctions = (SaLDec) returnValue;
         }
 
@@ -26,77 +38,196 @@ public class Sc2sa extends DepthFirstAdapter {
     }
 
     @Override
-    public void caseAVarGlobDecVarGlob(AVarGlobDecVarGlob node) {
+    public void caseANoVarProgram(ANoVarProgram node) {
+        SaLDec fonctions = null;
 
+        if(node.getDecLFunc() != null) {
+            node.getDecLFunc().apply(this);
+            fonctions = (SaLDec) returnValue;
+        }
+
+        returnValue = new SaProg(null, fonctions);
+    }
+
+
+
+    // B
+    @Override
+    public void caseAVarDecLVar(AVarDecLVar node) {
+        SaDec decVar = null;
+        SaLDec lDec = null;
+
+        if(node.getDecVar() != null) {
+            node.getDecVar().apply(this);
+            decVar = (SaDec) returnValue;
+        }
+        if(node.getDecLVarB() != null) {
+            node.getDecLVarB().apply(this);
+            lDec = (SaLDec) returnValue;
+        }
+
+        returnValue = new SaLDec(decVar, lDec);
     }
 
     @Override
-    public void caseANoVarGlobDecVarGlob(ANoVarGlobDecVarGlob node) {
-
+    public void caseANoVarDecLVar(ANoVarDecLVar node) {
+        returnValue = null;
     }
 
     @Override
-    public void caseAVarDecListVar(AVarDecListVar node) {
+    public void caseAVarDecLVarB(AVarDecLVarB node) {
+        SaDec decVar = null;
+        SaLDec lDec = null;
 
+        if(node.getVirg() != null) {
+            node.getVirg().apply(this);
+        }
+        if(node.getDecVar() != null) {
+            node.getDecVar().apply(this);
+            decVar = (SaDec) returnValue;
+        }
+        if(node.getDecLVarB() != null) {
+            node.getDecLVarB().apply(this);
+            lDec = (SaLDec) returnValue;
+        }
+
+        returnValue = new SaLDec(decVar, lDec);
     }
 
     @Override
-    public void caseAListVarDecListVar(AListVarDecListVar node) {
-
-    }
-
-    @Override
-    public void caseANoVarDecListVar(ANoVarDecListVar node) {
-
+    public void caseAEndDecLVarB(AEndDecLVarB node) {
+        returnValue = null;
     }
 
     @Override
     public void caseAEntierDecVar(AEntierDecVar node) {
+        String nom = null;
 
+        if(node.getEntier() != null) {
+            node.getEntier().apply(this);
+        }
+        if(node.getIdent() != null) {
+            node.getIdent().apply(this);
+            nom = node.getIdent().getText();
+        }
+
+        returnValue = new SaDecVar(nom);
     }
 
     @Override
     public void caseATabDecVar(ATabDecVar node) {
+        String nom = null;
+        int taille = 0;
+
+        if(node.getIdent() != null) {
+            node.getIdent().apply(this);
+            nom = node.getIdent().getText();
+        }
+
+        if(node.getConst() != null)
+        {
+            node.getConst().apply(this);
+            taille = Integer.parseInt(node.getConst().getText());
+        }
+
+        returnValue = new SaDecTab(nom, taille);
+    }
+
+
+
+    // C
+
+    @Override
+    public void caseAFuncDecLFunc(AFuncDecLFunc node) {
+        SaDec fonc = null;
+        SaLDec lFonc = null;
+
+        if(node.getDecFunc() != null) {
+            node.getDecFunc().apply(this);
+            fonc = (SaDec) returnValue;
+        }
+        if(node.getDecLFunc() != null) {
+            node.getDecLFunc().apply(this);
+            lFonc = (SaLDec) returnValue;
+        }
+
+        returnValue = new SaLDec(fonc, lFonc);
+    }
+
+    @Override
+    public void caseANoFuncDecLFunc(ANoFuncDecLFunc node) {
+        returnValue = null;
+    }
+
+    @Override
+    public void caseAWithVarDecFunc(AWithVarDecFunc node) {
+        String nom = null;
+        SaLDec params = null;
+        SaLDec vars = null;
+        SaInst insts = null;
+
+        if(node.getIdent() != null) {
+            node.getIdent().apply(this);
+            nom = node.getIdent().getText();
+        }
+        if(node.getArgs() != null) {
+            node.getArgs().apply(this);
+            params = (SaLDec) returnValue;
+        }
+        if(node.getVars() != null) {
+            node.getVars().apply(this);
+            vars = (SaLDec) returnValue;
+        }
+        if(node.getBloc() != null) {
+            node.getBloc().apply(this);
+            insts = (SaInst) returnValue;
+        }
+
+        returnValue = new SaDecFonc(nom, params, vars, insts);
+    }
+
+    @Override
+    public void caseANoVarDecFunc(ANoVarDecFunc node) {
+        String nom = null;
+        SaLDec params = null;
+        SaInst insts = null;
+
+        if(node.getIdent() != null) {
+            node.getIdent().apply(this);
+            nom = node.getIdent().getText();
+        }
+        if(node.getDecLVar() != null) {
+            node.getDecLVar().apply(this);
+            params = (SaLDec) returnValue;
+        }
+        if(node.getBloc() != null) {
+            node.getBloc().apply(this);
+            insts = (SaInst) returnValue;
+        }
+
+        returnValue = new SaDecFonc(nom, params, null, insts);
+    }
+
+
+
+    // D
+
+    @Override
+    public void caseABloc(ABloc node) {
+        SaLInst // BRUH
+
+        if(node.getLInst() != null) {
+            node.getLInst().apply(this);
+        }
+    }
+
+    @Override
+    public void caseAInstLInst(AInstLInst node) {
 
     }
 
     @Override
-    public void caseAListFoncDecListFonc(AListFoncDecListFonc node) {
-
-    }
-
-    @Override
-    public void caseANoFoncDecListFonc(ANoFoncDecListFonc node) {
-
-    }
-
-    @Override
-    public void caseADecFoncDecFonc(ADecFoncDecFonc node) {
-
-    }
-
-    @Override
-    public void caseADecListFoncOptionDecListFoncOption(ADecListFoncOptionDecListFoncOption node) {
-
-    }
-
-    @Override
-    public void caseANoDecListFoncOptionDecListFoncOption(ANoDecListFoncOptionDecListFoncOption node) {
-
-    }
-
-    @Override
-    public void caseABlockBloc(ABlockBloc node) {
-
-    }
-
-    @Override
-    public void caseAInstInstList(AInstInstList node) {
-
-    }
-
-    @Override
-    public void caseANoInstInstList(ANoInstInstList node) {
+    public void caseANoInstLInst(ANoInstLInst node) {
 
     }
 
@@ -121,7 +252,7 @@ public class Sc2sa extends DepthFirstAdapter {
     }
 
     @Override
-    public void caseAFoncInst(AFoncInst node) {
+    public void caseAFuncInst(AFuncInst node) {
 
     }
 
@@ -131,82 +262,77 @@ public class Sc2sa extends DepthFirstAdapter {
     }
 
     @Override
-    public void caseAAffIAff(AAffIAff node) {
+    public void caseAIAff(AIAff node) {
 
     }
 
     @Override
-    public void caseASiISi(ASiISi node) {
+    public void caseAWithSinonISi(AWithSinonISi node) {
 
     }
 
     @Override
-    public void caseASinonISinon(ASinonISinon node) {
+    public void caseANoSinonISi(ANoSinonISi node) {
 
     }
 
     @Override
-    public void caseANoSinonISinon(ANoSinonISinon node) {
+    public void caseAITantque(AITantque node) {
 
     }
 
     @Override
-    public void caseATantqueITantque(ATantqueITantque node) {
+    public void caseAIRetour(AIRetour node) {
 
     }
 
     @Override
-    public void caseARetourIRetour(ARetourIRetour node) {
+    public void caseAAppFuncIFunc(AAppFuncIFunc node) {
 
     }
 
     @Override
-    public void caseAAppFoncIFonc(AAppFoncIFonc node) {
+    public void caseAAppLireIFunc(AAppLireIFunc node) {
 
     }
 
     @Override
-    public void caseAAppLireIFonc(AAppLireIFonc node) {
+    public void caseAIEcrire(AIEcrire node) {
 
     }
 
     @Override
-    public void caseAAppEcrireIEcrire(AAppEcrireIEcrire node) {
+    public void caseAEntierVar(AEntierVar node) {
 
     }
 
     @Override
-    public void caseAIdentVarVar(AIdentVarVar node) {
+    public void caseATabEntierVar(ATabEntierVar node) {
 
     }
 
     @Override
-    public void caseAIdentTabVar(AIdentTabVar node) {
+    public void caseAFunc(AFunc node) {
 
     }
 
     @Override
-    public void caseAFoncFonc(AFoncFonc node) {
+    public void caseAArgFuncLArg(AArgFuncLArg node) {
 
     }
 
     @Override
-    public void caseAArgsArgs(AArgsArgs node) {
+    public void caseANoArgFuncLArg(ANoArgFuncLArg node) {
 
     }
 
     @Override
-    public void caseANoArgsArgs(ANoArgsArgs node) {
+    public void caseAExpArg(AExpArg node) {
 
     }
 
     @Override
-    public void caseAArgArg(AArgArg node) {
-
-    }
-
-    @Override
-    public void caseANoArgArg(ANoArgArg node) {
+    public void caseANoExpArg(ANoExpArg node) {
 
     }
 
@@ -296,7 +422,7 @@ public class Sc2sa extends DepthFirstAdapter {
     }
 
     @Override
-    public void caseAFoncExp6(AFoncExp6 node) {
+    public void caseAFuncExp6(AFuncExp6 node) {
 
     }
 
@@ -469,5 +595,4 @@ public class Sc2sa extends DepthFirstAdapter {
     public void caseInvalidToken(InvalidToken node) {
 
     }
-
 }
